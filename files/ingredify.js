203dbb604eeb913de9844719d1378158906d1e0a
Philip Webster <phil@remind101.com>
2016-11-24 12:38:51 -0800

    log selection

--- files/ingredify.js
+++ files/ingredify.js
@@ -1,4 +1,29 @@
 
 function ingredify(){
-	console.log("hello");
+	var text = getSelectionText();
+	console.log(text);
+}
+
+function getSelectionText() {
+    var text = "";
+    if (typeof window.getSelection != "undefined") {
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+            var container = document.createElement("div");
+            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                container.appendChild(sel.getRangeAt(i).cloneContents());
+            }
+            text = container.innerText || container.textContent;
+        }
+    } else if (typeof document.selection != "undefined") {
+        if (document.selection.type == "Text") {
+            text = document.selection.createRange().text;
+        }
+    }
+    if(text === ""){
+        return false;
+    }
+    else{
+        return text;
+    }
 }
